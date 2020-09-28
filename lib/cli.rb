4fa238fa5eade93d6e288570d3d1cfbe8f4583af
LeahESc <leahemily1@gmail.com>
2020-09-28 09:31:56 -0700

    welcomes user, asks for input

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,5 +4,8 @@ class CLI
 
     def menu
         puts "Hello, I'm Marian, the Librarian."
+        # book illustration 
+        puts "What type of book are you looking for? Type the name of a genre to search by genre or the name of an author to see all books by that author. To exit, type 'Goobye Marian'." 
+
     end
 end 
