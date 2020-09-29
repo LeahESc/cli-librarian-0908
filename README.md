cc9111b63f13eecaa6fc2a2f619fda402f12a33c
LeahESc <leahemily1@gmail.com>
2020-09-29 13:59:29 -0700

    creates lots of new methods for input and list

---
+++ README.md
@@ -0,0 +1,4 @@
+this app does ... 
+it can be found at github/repo
+to run it you need to install the following gems: 
+etc etc 
--- lib/cli.rb
+++ lib/cli.rb
@@ -1,41 +1,64 @@
-require 'pry'
-
 class CLI 
 
     def run
+        # binding.pry
+        # binding.pry
         puts ""
         puts "Hello, I'm Marian the Librarian! Welcome to my library!"
         puts ""
         puts "----------------------------------"
+        # binding.pry 
+        menu_loop
         # book illustration
-        
-        puts "To search for a list of books from an author, type the author's first and last name.To exit, type 'Goobye Marian'." 
+        # puts "To search for a list of books from an author, type the author's first and last name.To exit, type 'exit'." 
+        # puts ""
+        # get_input
+        # while @input[0] != "exit"
+            # set_input_to_name
+            # if Book.find_by_author(@f_name, @l_name).count > 0
+            #     print_titles_by_author(@f_name, @l_name)
+            #     puts ""
+            #     book_prompt
+            #     book_selection
+                
+            # elsif Book.find_by_author(@f_name, @l_name).count == 0
+            #     puts "I'm sorry, I couldn't understand that. Would you like to try again?"
+            #     author_prompt
+            # else
+            #     puts "I'm sorry, I couldn't understand that. Would you like to try again?" 
+            #     puts ""
+            # end
+            # author_prompt
+            # input = gets.strip.downcase.split(" ")
+            # API.get_books(@f_name, @l_name)     
+        # end 
+    end
+
+    def menu_loop
+        puts "To search for a list of books from an author, type the author's first and last name.To exit, type 'exit'." 
         puts ""
-        input = gets.strip.downcase.split(" ")
-        while input[0] != "exit"
-            input[0] = @f_name
-            input[1] = @l_name
-            API.get_books(@f_name, @l_name) 
-                if Book.find_by_author(@f_name, @l_name).count > 0
-                    print_titles_by_author(@f_name, @l_name)
-                    puts ""
-                    book_selection
-                   
-                elsif Book.find_by_author(@f_name, @l_name).count == 0
-                    puts "I'm sorry, I couldn't understand that. Would you like to try again?"
-                    author_prompt
-                else
-                    puts "I'm sorry, I couldn't understand that. Would you like to try again?" 
-                    puts ""
-                end
+        binding.pry
+        get_input
+        while @input[0] != "exit"
+            set_input_to_name
+            if Book.find_by_author(@f_name, @l_name).count > 0
+                print_titles_by_author(@f_name, @l_name)
+                puts ""
                 book_prompt
+                book_selection
+                
+            elsif Book.find_by_author(@f_name, @l_name).count == 0
+                puts "I'm sorry, I couldn't understand that. Would you like to try again?"
                 author_prompt
-                input = gets.strip.downcase.split(" ")
-                API.get_books(@f_name, @l_name) 
-               
+            else
+                puts "I'm sorry, I couldn't understand that. Would you like to try again?" 
+                puts ""
             end
+            author_prompt
+            input = gets.strip.downcase.split(" ")
+            API.get_books(@f_name, @l_name)  
         end
-        exit  
+        goodbye
     end
 
 
@@ -43,8 +66,6 @@ class CLI
         titles = Book.find_by_author(@f_name, @l_name)
         titles.each.with_index(1) do |t,i|
             puts "#{i}. #{t.title.gsub(/\w+/){|word| word.capitalize}}"
-            puts ""
-            puts "To learn more about a book, type the number next to a title"
         end
     end
 
@@ -55,40 +76,56 @@ class CLI
 
     def author_prompt
         puts "To search by author, type an author's first and last name."
-        puts "To exit, type 'Goodbye Marian'"  
+        puts "To exit, type 'exit'"  
         puts ""
     end
 
     def book_selection
         input = gets.strip.downcase
         while input != "menu"
-        if input[0].to_i > 0 && input[0].to_i <= Book.find_by_author(@f_name, @l_name).count
-            book = Book.find_by_author(@f_name, @l_name)[input[0].to_i - 1]
-            print_book_details(book)
-        elsif 
-            puts "I'm sorry, I couldn't understand that. Would you like to try again?"
+            if input[0].to_i > 0 && input[0].to_i <= Book.find_by_author(@f_name, @l_name).count
+                book = Book.find_by_author(@f_name, @l_name)[input[0].to_i - 1]
+                print_book_details(book)
+                puts ""
+                puts "Can I help you find anything else?"
+                puts "To got back to the main menu, type 'menu'"
+            elsif 
+                puts "I'm sorry, I couldn't understand that. To see the list of book titles again type 'list'." 
+                print_titles_by_author(@f_name, @l_name)
+            end
         end
         menu_loop 
     end 
+
+    def list 
+        get_input
+        while @input != menu
+            if @input == "list"
+            print_titles_by_author(@f_name, @l_name)
+            else
+            puts "I'm sorry, I couldn't understand that. To go back to the main menu, type 'menu'."
+            end
+            menu_loop
+        end
+    end
+
+    def get_input 
+        @input = gets.strip.downcase.split(" ")
+    end
+
+    def set_input_to_name
+        @input[0] = @f_name 
+        @input[1] = @l_name
+    end
 
     def print_book_details(book)
         puts "Title: #{book.title}"
         puts "First year published: #{book.publish_date}"
         puts "First sentence: #{book.first_sentence.join(" ")}" 
         puts "Subject(s): #{book.subject.join("\n")}"
-        puts "Can I help you find anything else?"
-        puts "To search for books by another author type 'menu'"
     end
 
-    def exit
+    def goodbye
         puts "Goodbye! Thanks for stopping by!"
     end
-
-    # def valid? 
-    #     valid_books = Book.find_by_author(@f_name, @l_name) 
-    #     # binding.pry
-    #     if valid_books.count == 0
-    #         puts "I'm sorry, I don't recognize that name. Would you like to try again?"
-    #     end
-    # end
 end 
