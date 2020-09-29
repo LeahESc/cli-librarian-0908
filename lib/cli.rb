3449da212d07354228c484c06d3d056120236b11
LeahESc <leahemily1@gmail.com>
2020-09-29 11:17:09 -0700

    code pulls random books

--- lib/cli.rb
+++ lib/cli.rb
@@ -10,66 +10,51 @@ class CLI
         # book illustration 
         puts "To search for a list of books from an author, type the author's first and last name.To exit, type 'Goobye Marian'." 
         puts ""
-        # input = gets.strip.downcase.split(" ")
-        # binding.pry
-        # if input[0] == "exit"
-        #     exit
-        # else 
-            # @f_name = input[0]
-            # @l_name = input[1]
-            # API.get_books(@f_name, @l_name)
-            # puts ""
-            # binding.pry
-            # while Book.find_by_author(@f_name, @l_name).count == 0
-            # if Book.find_by_author(@f_name,@l_name).count == 0
-            #     prompt_one
-            # else
-            #     print_titles_by_author(@f_name, @l_name)
-            
-            #     puts ""
-            #     prompt_two
+        input = gets.strip.downcase.split(" ")
+        while input[0] != "exit"
+            @f_name = input[0]
+            @l_name = input[1]
+            API.get_books(@f_name, @l_name) 
+                if input[0].to_i > 0 && input[0].to_i <= Book.find_by_author(@f_name, @l_name).count
+                    book = Book.find_by_author(@f_name, @l_name)[input[0].to_i - 1]
+                    print_book_details(book)
+                    puts "Can I help you find anything else?"
+                    author_prompt
+                elsif Book.find_by_author(@f_name, @l_name).count > 0
+                    # input[0] == f_name && input[1] == l_name
+                    print_titles_by_author(@f_name, @l_name)
+                    puts ""
+                elsif Book.find_by_author(@f_name, @l_name).count == 0
+                    puts "I'm sorry, I couldn't understand that. Would you like to try again?"
+                    author_prompt
+                else
+                    puts "I'm sorry, I couldn't understand that. Would you like to try again?" 
+                    puts ""
+                end
+                book_prompt
+                author_prompt
                 input = gets.strip.downcase.split(" ")
                 @f_name = input[0]
                 @l_name = input[1]
-                API.get_books(@f_name, @l_name) if Book.find_by_author(@f_name, @l_name).count == 0
-                # binding.pry
-                while input != "exit"
-                    if input[0].to_i > 0 && input.to_i <= Book.find_by_author(@f_name, @l_name).count
-                        book = Book.find_by_author(@f_name, @l_name)[input[0].to_i - 1]
-                        # API.get_book(book)
-                        print_book_details(book)
-                        prompt_two
-                    elsif Book.find_by_author(@f_name, @l_name).count > 0
-                        # input[0] == f_name && input[1] == l_name
-                        print_titles_by_author(@f_name, @l_name)
-                    else
-                        puts "I'm sorry, I couldn't understand that. Would you like to try again? Try typing the number of one of the books listed to see more information."
-                        puts ""
-                    end
-                    prompt_two
-                    input = gets.strip.downcase.split(" ")
-                    @f_name = input[0]
-                    @l_name = input[1]
-                    API.get_books(@f_name, @l_name) if Book.find_by_author(@f_name, @l_name).count == 0
+                API.get_books(@f_name, @l_name) 
                 end
-        #     end
         # end  
     end
 
     def print_titles_by_author(f_name, l_name)
         titles = Book.find_by_author(@f_name, @l_name)
-         titles.each.with_index(1) do |t,i|
-                puts "#{i}. #{t.title.gsub(/\w+/){|word| word.capitalize}}"
-            end
+        titles.each.with_index(1) do |t,i|
+            puts "#{i}. #{t.title.gsub(/\w+/){|word| word.capitalize}}"
+        end
     end
 
-    def prompt_one
-        puts "If you'd like to search for books by a different author, please type the author's first and last name."
+    def book_prompt
+        puts "To learn more about a book, type the number listed next to a title"
+        puts ""
     end
 
-    def prompt_two 
-        puts "To learn more about a particular book, please type the number listed next to the title." 
-        puts "If you'd like to search for books by an author, please type the author's first and last name."
+    def author_prompt
+        puts "To search by author, type an author's first and last name."
         puts "To exit, type 'Goodbye Marian'"  
         puts ""
     end
@@ -77,7 +62,7 @@ class CLI
     def print_book_details(book)
         puts "Title: #{book.title}"
         puts "First year published: #{book.publish_date}"
-        puts "First sentence: #{book.first_sentence.join(" ")}"
+        puts "First sentence: #{book.first_sentence.join(" ")}" if != nil
         puts "Subject(s): #{book.subject.join("\n")}"
     end
 
@@ -85,11 +70,11 @@ class CLI
         puts "Goodbye! Thanks for stopping by!"
     end
 
-    def valid? 
-        valid_books = Book.find_by_author(@f_name, @l_name) 
-        # binding.pry
-        if valid_books.count == 0
-            puts "I'm sorry, I don't recognize that name. Would you like to try again?"
-        end
-    end
+    # def valid? 
+    #     valid_books = Book.find_by_author(@f_name, @l_name) 
+    #     # binding.pry
+    #     if valid_books.count == 0
+    #         puts "I'm sorry, I don't recognize that name. Would you like to try again?"
+    #     end
+    # end
 end 
